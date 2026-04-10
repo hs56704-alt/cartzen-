bad68883853aa92922ea960ddb698c9a844e50ea
Himanshu Singh <hs56704@gmail.com>
2026-04-10 15:51:49 +0530

    sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -7,6 +7,7 @@ import { headerData } from '@/constants';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import SocialMedia from './SocialMedia';
+import { useOutsideClick } from '@/hooks/useOutsideClick';
 
 
 interface SidebarProps{
@@ -16,10 +17,16 @@ interface SidebarProps{
 
 const SideBar: FC<SidebarProps> = ({ isOpen, onClose }) =>{
   const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
-    <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect w-full ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
+    <div className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect cursor-auto w-full ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
       
-        < motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition ={{duration:0.4,delay:0.3 }}className='min-w-72 max-w-96 bg-darkColor text-white/70 h-full p-10 border-r border-r-white flex flex-col gap-6'>
+        < motion.div 
+        initial={{opacity: 0}} 
+        animate={{opacity: 1}} 
+        transition ={{duration:0.4,delay:0.3 }}
+        ref={sidebarRef}
+        className='min-w-72 max-w-96 bg-darkColor text-white/70 h-full p-10 border-r border-r-white flex flex-col gap-6'>
           <div className='flex items-center justify-between '>
             <button onClick={onClose}>
             <Logo >
@@ -41,7 +48,7 @@ const SideBar: FC<SidebarProps> = ({ isOpen, onClose }) =>{
             {headerData?.map((item) => (
               <Link 
               onClick={onClose}
-              key={item?.href} href={item?.href} className={`hover:text-white hoverEffect ${pathname === item?.href && "text-white"}`}>
+              key={item?.href} href={item?.href} className={`hover:text-white hoverEffect w-24 ${pathname === item?.href && "text-white"}`}>
               {item?.title}
               </Link>
             ))}
--- hooks/useOutsideClick.ts
+++ hooks/useOutsideClick.ts
@@ -15,7 +15,7 @@ export function useOutsideClick<T extends HTMLElement>(callback: () => void) {
         };
     }, [callback]);
     
-    
+     
     return ref;
 
 } 
